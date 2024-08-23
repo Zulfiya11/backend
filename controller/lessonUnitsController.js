a603291cf5fc52224a198563bcc7fd3a0592de25
Otabek Atakhanov <otaxonov137@gmail.com>
2024-08-23 17:48:55 +0500

    23.08.2

--- controller/lessonUnitsController.js
+++ controller/lessonUnitsController.js
@@ -1,31 +1,34 @@
-const Lesson_units = require('../models/lesson_units')
+const Lesson_units = require("../models/lesson_units");
 
-exports.createLessonUnit = async(req, res) => {
-
+exports.createLessonUnit = async (req, res) => {
+  for (let i = 0; i < req.body.selected_subjects.length; i++) {
+     
     await Lesson_units.query().insert({
-       lesson_id: req.body.lesson_id,
-       unit_id: req.body.unit_id,
-       module_id: req.body.module_id,
-       status: "active"
-    })
-
-    return res.status(201).json({ success: true, msg: 'Lesson yaratildi' })
-}
-
-exports.getAllLessonUnits = async(req,res) => {
-    const lesson_unit = await Lesson_units.query().where('module_id', req.params.id)
-    return res.json({success:true, lesson_units: lesson_unit})
-}
-
-exports.editLessonUnit = async(req,res) => {
-    await Lesson_units.query().where('id', req.params.id).update({
         lesson_id: req.body.lesson_id,
-        unit_id: req.body.unit_id
-    })
-}
+        unit_id: req.body.selected_subjects[i],
+        module_id: req.body.module_id,
+        status: "active",
+      });
+  }
 
-exports.deleteLessonUnit = async(req,res) => {
-    await Lesson_units.query().where('id', req.params.id).update({
-        status: "deleted"
-    })
-}
+
+  return res.status(201).json({ success: true, msg: "Lesson Unit yaratildi" });
+};
+
+exports.getAllLessonUnits = async (req, res) => {
+  const lesson_unit = await Lesson_units.query().where("module_id",req.params.id);
+  return res.json({ success: true, lesson_units: lesson_unit });
+};
+
+exports.editLessonUnit = async (req, res) => {
+  await Lesson_units.query().where("id", req.params.id).update({
+    lesson_id: req.body.lesson_id,
+    unit_id: req.body.unit_id,
+  });
+};
+
+exports.deleteLessonUnit = async (req, res) => {
+  await Lesson_units.query().where("id", req.params.id).update({
+    status: "deleted",
+  });
+};

85bbbd103cb1f884443ec58b1140e66b47129b34
Otabek Atakhanov <otaxonov137@gmail.com>
2024-08-27 16:04:50 +0500

    27.08.2

---
+++ router/groupEnrolements.js
@@ -0,0 +1,20 @@
+const router = require('express').Router()
+const groupEnrolementsController = require('../controller/groupEnrolementsController')
+
+
+
+router.post('/create', groupEnrolementsController.createGroupEnrolement)
+
+router.get('/all', groupEnrolementsController.getaAllGroupEnrolements)
+
+router.post('/edit/:id', groupEnrolementsController.editGroupEnrolement)
+
+router.post('/delete/:id', groupEnrolementsController.deleteGroupEnrolement)
+
+
+
+module.exports = router
+
+
+
+
